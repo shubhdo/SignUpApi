f8995778ee02f5b9acc2650926680346048778d6
shubham shukla <shubhamshukla.shukla@gmail.com>
2017-08-25 10:45:31 +0530

    route for handling image upload

--- User.js
+++ User.js
@@ -2,63 +2,65 @@ const mongoose = require('mongoose');
 
 const Schema=mongoose.Schema;
 
+
 const User=new Schema({
-   firstname:{
-       type:String,
-       required:[true,'Please enter the first name'],
-       match:/^[A-Za-z]+$/,
-       trim:true
-   },
+    firstname:{
+        type:String,
+        required:[true,'Please enter the first name'],
+        match:[/^[A-Za-z]+$/,'Only alphabets are allowed'],
+        trim:true
+    },
     lastname:{
         type:String,
         required:[true,'Please enter the last name'],
-        match:/^[A-Za-z]+$/,
+        match:[/^[A-Za-z]+$/,'Only alphabets are allowed'],
         trim:true
     },
     email:{
         type:String,
-        required:[true,'Please enter the valid email'],
-        match:/\S+@\S+\.\S+/,
-        unique:true,
+        required:[true,'Please enter the email'],
+        match:[/\S+@\S+\.\S+/,'Please enter a valid email'],
+        unique:[true,'email already registered'],
         trim:true
     },
     gender:{
         type:String,
-        required:true,
-        enum:['male','female','Others']
+        required:[true,'Please enter the gender(male or female)'],
+        enum:['male','female']
     },
     dob:{
         type:Date,
-        required:true,
+        required:[true,'Please enter DOB'],
     },
     country:{
         type:String,
+        required:[true,'Please enter the country'],
         trim:true,
-        match:/^[A-Za-z ]+$/,
+        match:[/^[A-Za-z ]+$/,'Only Alphabets are allowed']
 
     },
     state:{
         type:String,
+        required:[true,'Please enter the state'],
         trim:true,
-        match:/^[A-Za-z ]+$/,
+        match:[/^[A-Za-z ]+$/,'Only Alphabets are allowed']
 
     },
     city:{
         type:String,
-        match:/^[A-Za-z ]+$/,
+        required:[true,'Please enter the city'],
+        match:[/^[A-Za-z ]+$/,'Only Alphabets are allowed'],
         trim:true
     },
     contact:{
         type:Number,
-        match:/^[0-9]{10,14}$/,
+        required:[true,'Please enter the contact no'],
+        match:[/^[0-9]{10,14}$/,'Only numeric value is allowed(10-14 digits only']
 
     },
-    profilePic:{
-        data:Buffer,
-        contentType:String
-    },
+
     imagePath:{
-       type:String
+        type:String
     }
 });
 
--- index.js
+++ index.js
@@ -40,79 +40,84 @@ app.post('/addUser', function (req, res) {
         contact: req.body.contact
 
     });
-
-    async.waterfall([
-        function (callback) {
-            user.save(function (error, result) {
-                if (error) {
-
-                    console.log(error.message);
-                    callback(error.message);
-                }
-                else {
-                    console.log("---------------", result);
-                    callback(null, result)
-
-                }
-            })
-        },
-        function (result, callback) {
-            let requestSettings = {
-                url: req.body.url,
-                method: 'GET',
-                encoding: null
-            };
-
-            request(requestSettings, function (error, response, body) {
-
-                let filePath = './images/img' + new Date().getTime() + ".jpg";
-                fs.writeFile(filePath, body, 'ascii', function (err) {
-                    if (err) {
-                        console.log(err);
-                        callback(err);
-                    }
-                    else {
-                        callback(null, result, filePath)
-                    }
-                });
-
-            });
-
-        },
-        function (result, path, callback) {
-            User.findOneAndUpdate({_id: result._id}, {
-                $set: {
-                    imagePath: path
-                }
-            }, function (er, re) {
-                if (er) {
-                    console.log(er);
-                    callback(er);
-                }
-                else {
-                    console.log("*******************", re);
-                    callback(null, re)
-
-                }
-            })
-        }
-    ], function (error, results) {
+    user.save(function (error, result) {
         if (error) {
             console.log(error);
-            res.status(400).send({"error": error});
+            res.status(400).send({
+                responseCode: 400,
+                result: error.message
+            });
         }
         else {
-            console.log("+++++++++++++++++++", results);
+            console.log("---------------", result);
             res.status(200).send({
                 responseCode: 200,
                 responseMessage: 'User Added Successfully',
-                result: results
+                result: result
             });
+
         }
     })
 });
+/*function (result, callback) {
+   let requestSettings = {
+        url: req.body.url,
+        method: 'GET',
+        encoding: null
+    };
 
-app.post('/saveImageInBase64', function (req, res) {
+    request(requestSettings, function (error, response, body) {
+
+        let filePath = './images/img' + new Date().getTime() + ".jpg";
+        fs.writeFile(filePath, body, 'ascii', function (err) {
+            if (err) {
+                console.log(err);
+                callback(err);
+            }
+            else {
+                callback(null, result, filePath)
+            }
+        });
+
+    });
+
+
+
+},*/
+/* function (result, path, callback) {
+     User.findOneAndUpdate({_id: result._id}, {
+         $set: {
+             imagePath: path
+         }
+     }, function (er, re) {
+         if (er) {
+             console.log(er);
+             callback(er);
+         }
+         else {
+             console.log("*******************", re);
+             callback(null, re)
+
+         }
+     })
+ }*/
+/*  ], function (error, results) {
+      if (error) {
+          console.log(error);
+          res.status(400).send({"error": error});
+      }
+      else {
+          console.log("+++++++++++++++++++", results);
+          res.status(200).send({
+              responseCode: 200,
+              responseMessage: 'User Added Successfully',
+              result: results
+          });
+      }
+  })
+});*/
+
+/*app.post('/saveImageInBase64', function (req, res) {
     let filePath = './images64/img' + new Date().getTime();
     let requestSettings = {
         url: req.body.url,
@@ -147,7 +152,7 @@ app.get('/img', function (req, res) {
         res.send(success.profilePic.data);
     })
 
-})
+})*/
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -161,7 +166,7 @@ let storage = multer.diskStorage({
 let upload = multer({storage: storage, encoding: "base64"}).single('file');
 
 
-function encodeAndSaveImage(path, contentType) {
+/*function encodeAndSaveImage(path, contentType) {
     let temp;
     fs.readFile(path, function (err, data) {
             if (err)
@@ -191,27 +196,27 @@ app.post('/saveImage', function (req, res) {
             res.send("File is uploaded");
         encodeAndSaveImage(req.file.path, req.file.mimetype);
     })
-});
+});*/
 
 
 app.post("/upload", multer({dest: "./upload/"}).single('file'), function (req, res) {
-    let path='./upload/img' + new Date().getTime();
-    console.log(req.body)
+    let path = './upload/img' + new Date().getTime()+".jpg";
+    console.log(req.body);
     async.waterfall([
         function (callback) {
-            let temp = new Buffer(fs.readFileSync(req.file.path)).toString("base64");
+            let temp = "data:" + req.file.mimetype + ";base64," + new Buffer(fs.readFileSync(req.file.path)).toString("base64");
             fs.writeFile(path, temp, null, function (err) {
                 if (err) {
                     callback(err);
                 }
                 else {
-                    callback(null,path)
-                    console.log("Scucessfully converted")
+                    callback(null, path)
+                    console.log("Sucessfully converted")
                 }
             });
             fs.unlink(req.file.path);
         },
-        function (path,callback) {
+        function (path, callback) {
             User.findOneAndUpdate({_id: req.body.id}, {
                 $set: {
                     imagePath: path
@@ -228,7 +233,7 @@ app.post("/upload", multer({dest: "./upload/"}).single('file'), function (req, r
                 }
             })
         }
-    ],function (error, results) {
+    ], function (error, results) {
         if (error) {
             console.log(error);
             res.status(400).send({"error": error});
@@ -244,6 +249,86 @@ app.post("/upload", multer({dest: "./upload/"}).single('file'), function (req, r
     })
 
 });
+
+/*
+app.get('/upload/:imgName',function (req,res) {
+    console.log(req.params.imgName)
+   if (req.params.imgName===undefined) {
+       res.status(400).send({"error":"send valid name"});
+       return;
+   }
+    res.setHeader('content-type', 'image/jpeg');
+    let img = new Buffer(fs.readFileSync('./upload/'+req.params.imgName), 'base64');
+    res.end(img)
+});
+*/
+app.post("/save", multer({dest: "./upload/"}).single('file'), function (req, res) {
+    let data =fs.readFileSync(req.file.path,'utf-8');
+    let imageBuffer = decodeBase64Image(data);
+    let paths = './image/img' + new Date().getTime()+".jpg";
+    async.waterfall([
+        function (callback) {
+            fs.writeFile(paths, imageBuffer.data, function(err) {
+                if (err) {
+                    console.log(err);
+                    callback(err);
+                }
+                else {
+                    callback(null, paths);
+                    console.log("Sucessfully converted")
+
+                }
+            });
+            fs.unlink(req.file.path);
+        },
+        function (path, callback) {
+            User.findOneAndUpdate({_id: req.body.id}, {
+                $set: {
+                    imagePath: path
+                }
+            }, function (er, re) {
+                if (er) {
+                    console.log(er);
+                    callback(er);
+                }
+                else {
+                    console.log("*******************", re);
+                    callback(null, re)
+
+                }
+            })
+        }
+    ], function (error, results) {
+        if (error) {
+            console.log(error);
+            res.status(400).send({"error": error});
+        }
+        else {
+            console.log("+++++++++++++++++++", results);
+            res.status(200).send({
+                responseCode: 200,
+                responseMessage: 'Image Added Successfully',
+                result: results
+            });
+        }
+    })
+
+
+});
+
+function decodeBase64Image(dataString) {
+    let matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+    if (matches.length !== 3) {
+        return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+}
 
 
 app.listen(3001, function () {
